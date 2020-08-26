457316d03fa4010adbbc89c1006a2fcfe301e1af
Stanislav Lvovsky <stanislav.lvovsky@sap.com>
2020-08-26 11:30:45 +0300

    add deepEqual for type "list" (#57)
    * add deepEqual for type "list"
    
    * Update package.json
    
    * Update Form.vue

--- src/Form.vue
+++ src/Form.vue
@@ -37,6 +37,7 @@
 
 <script>
 import Plugins from "./Plugins";
+const assert = require('assert');
 
 const NOT_ANSWERED = "Mandatory field";
 export default {
@@ -217,7 +218,12 @@ export default {
           if (index < 0) {
             index = question._choices.findIndex(choice => {
               if (question._default) {
-                return choice.value === question._default;
+		  try {
+			  assert.deepEqual(choice.value, question._default);
+			  return true;
+		  } catch (error) {
+			  return false;
+		  } 
               }
             });
           }
@@ -297,8 +303,8 @@ export default {
         // call question methods or emit the answered event
         return;
       }
-      const index = this.questions.findIndex(value => {
-        return value["name"] === name;
+      const index = this.questions.findIndex(question => {
+        return question["name"] === name;
       });
 
       if (index === -1) {
@@ -338,9 +344,9 @@ export default {
               this.console.error(`Could not evaluate when() for ${question.name}`);
             }
           } else if (question.when !== false) {
-	      question.shouldShow = true;
-	      shouldValidate = true;
-	  }
+	     question.shouldShow = true;
+	     shouldValidate = true;
+	 }
 
           if (question.shouldShow) {
             // evaluate message()
@@ -451,15 +457,15 @@ export default {
 
         // default
         if (question.default !== undefined) {
-	  let _default;
-	  if (typeof question.default !== "function") {
+	   let _default;
+	   if (typeof question.default !== "function") {
 	     if (question.__origAnswer === undefined) {
 		_default = question.default;
 	     } else {
 		_default = question.__origAnswer; 
 	     }
-	  }
-	  this.$set(question, "_default", _default);
+	   }
+	   this.$set(question, "_default", _default);
 	} else if (question._default === undefined) {
 	   this.$set(question, "_default", question.__origAnswer);
 	}
