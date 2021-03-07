6d8001aeac7309289fd44852893b994219664401
Rima Sirich <rima.sirich@sap.com>
2021-03-07 17:26:16 +0200

    ci: finding components with `find` is deprecated

--- __tests__/confirm.spec.js
+++ __tests__/confirm.spec.js
@@ -17,7 +17,7 @@ describe('Question of type confirm', () => {
     wrapper.setProps({ questions: questionConfirm });
     await Vue.nextTick();
 
-    const confirm = wrapper.find('label');
+    const confirm = wrapper.findComponent('label');
     confirm.trigger('click');
 
     await Vue.nextTick();
