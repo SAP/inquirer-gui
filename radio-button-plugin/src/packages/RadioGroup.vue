c4019cfa415ebef7db52b5d4b70f990ba9691734
I061531 <vered.tagor@sap.com>
2023-03-19 10:04:32 +0200

    feat: add radio button plugin

---
+++ radio-button-plugin/src/packages/RadioGroup.vue
@@ -0,0 +1,43 @@
+<template>
+  <div v-if="isVisible()">
+    <v-radio-group
+      style="margin-bottom:0pt;padding-bottom:0pt"
+      :value="question.answer"
+      @change="onClick"
+      dense
+    >
+      <v-radio class="radioClass" v-for="item in question.choices" :key="item" :label="item" :value="item">{{item}}</v-radio>
+    </v-radio-group>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "RadioGroup",
+  props: {
+    question: Object
+  },
+  methods: {
+    getRadioId(name){
+        return "radio_"+ name;
+    },
+    onClick(answer) {
+      if (answer !== undefined) {
+        this.$emit("answerChanged", this.question.name, answer);
+      }
+    },
+    isVisible() {
+      return this.hasItem() && (this.question.visible!==false);
+    },
+    hasItem() {
+      return this.question.choices.length>0;
+    }
+  }
+};
+</script>
+<style>
+    .radioClass label {
+        font-size: 13px;
+        font-weight: 400;
+    }
+</style>
