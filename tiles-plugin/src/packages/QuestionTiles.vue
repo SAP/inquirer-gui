3bc94df17ce84665f421976aa52eab200c8f7f81
IainSAP <46536134+IainSAP@users.noreply.github.com>
2023-07-17 15:07:17 +0100

    Feat/add compact tiles (#637)
    * feat(tile-plugin): adds support for 'compact' tiles layout
    
    adds support via `guiOption.compact` to reduce space taken by tiles
    
    * fix(tiles-plugin): fix typo in elevation setting
    
    typo in v-card elevation lead to missing drop-shadow
    
    * chore(inquirer-gui): update versions, package lock
    
    * fix(tiles-plugin): update not applied css for compact container margins
    
    * chore(sample-app): updates QuestionSampleTiles copy
    
    * chore(sample-app): updates question tile copy name

--- tiles-plugin/src/packages/QuestionTiles.vue
+++ tiles-plugin/src/packages/QuestionTiles.vue
@@ -1,22 +1,22 @@
 <template>
   <div class="tiles">
     <v-item-group mandatory>
-      <v-container class="pa-0">
+      <v-container class="pa-0" :class="question.guiOptions && question.guiOptions.compact ? 'compact' : ''">
         <v-row class="tiles-row">
           <v-col
             v-for="(item, itemIndex) in question._choices"
             :key="itemIndex"
-            cols="12"
+            :cols="question.guiOptions && question.guiOptions.compact ? 'auto' : '12'"
+            :lg="question.guiOptions && question.guiOptions.compact ? '3' : '4'"
             md="4"
             sm="6"
           >
             <v-item v-slot:default="{ active, toggle }">
               <v-card
-                width="400"
-                class="d-flex flex-column mx-auto"
+                class="d-flex flex-column mx-auto tile-size"
+                :class="question.guiOptions && question.guiOptions.compact ? 'compact' : ''"
                 @click="onAnswerChanged(item.value)"
                 :data-itemvalue="item.value"
-                height="380"
                 tile
                 hover
                 flat
@@ -24,9 +24,11 @@
                 elevation=2
               >
                 <v-card-title>{{item.name}}</v-card-title>
-                <v-card-text class="description">
-                  {{item.description}}
-                </v-card-text>
+                <template>
+                  <v-card-text class="description" :class="question.guiOptions && question.guiOptions.compact ? 'compact' : ''">
+                    {{item.description}}
+                  </v-card-text>
+                </template>
                 <v-spacer></v-spacer>
                 <v-card-text class="homepage">
                   <a :href="item.homepage">More Information</a>
@@ -79,7 +81,7 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
 .v-card.selected {
   border: 1px solid;
 }
@@ -93,6 +95,14 @@ export default {
 .description.v-card__text {
   text-overflow: ellipsis;
   overflow: hidden;
+}
+.description.v-card__text.compact {
+  -webkit-line-clamp: 3;
+  -webkit-box-orient: vertical;
+  display: -webkit-box;
+  padding-bottom: 0px;
+  padding-top: 0px;
+  min-height: calc(1.375rem * 3) 
 }
 .homepage.v-card__text {
   padding-bottom: 0;
@@ -110,5 +120,18 @@ a {
   object-fit: scale-down;
   width: 100%;
   height: 100%;
+}
+.tile-size {
+  width: 400px;
+  height: 380px;
+}
+.tile-size.compact {
+  width: 300px;
+  height: 285px;
+  margin: 0px !important;
+}
+.container.compact {
+  margin-right: 0;
+  margin-left: 0;
 }
 </style>
