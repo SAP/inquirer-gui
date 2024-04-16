8a7b37fb7399f0ee94e0dd304903cea179902774
Alexander Gilin <alexander.gilin@sap.com>
2024-04-16 13:53:10 +0300

    fix: field validation condition is fixed if the “when” condition is defined

--- packages/inquirer-gui/src/Form.vue
+++ packages/inquirer-gui/src/Form.vue
@@ -451,9 +451,9 @@ export default {
               const whenPromise = question.when(answers);
               whenPromises.push(whenPromise);
               let response = await whenPromise;
-              // When question was shouldShow === false, and
-              //   it becomes shouldShow === true, call validate()
-              if (!question.shouldShow && response) {
+              // When question was shouldShow === false or question is invalid, and
+              // it becomes shouldShow === true, call validate()
+              if ((!question.shouldShow || !question.isValid) && response) {
                 shouldValidate = true;
               }
               question.shouldShow = response;
