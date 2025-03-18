1f0908153ec5f20358ec5d0b3be88e79e73e674a
Cristian Tirotta <59051220@users.noreply.github.com>
2025-03-18 10:45:08 +0200

    chore: removed unnecessary test overriding

--- packages/tiles-plugin/jest.config.js
+++ packages/tiles-plugin/jest.config.js
@@ -2,5 +2,4 @@ const baseConfig = require("../../jest.config.base.js");
 
 module.exports = {
   ...baseConfig,
-  testRegex: "(/packages/.*/__tests__/(.*).(test|spec)).[jt]sx?$" /* eslint-disable-line */,
 };
