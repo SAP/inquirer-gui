bd45256d3094a73fd96cd81aaf5b2f888932d254
Shahar Soel <shachar.soel@sap.com>
2025-05-27 17:14:06 +0300

    ci: re-enable github releases on tag pushes

---
+++ scripts/extract-last-ver-changelog.mjs
@@ -0,0 +1,24 @@
+import { resolve } from "node:path";
+import { readFile, writeFile } from "node:fs/promises";
+
+const __dirname = import.meta.dirname;
+const rootDir = resolve(__dirname, "..");
+
+const changelogPath = resolve(rootDir, "CHANGELOG.md");
+const changelog = await readFile(changelogPath, "utf-8");
+const isFirstVersion = changelog.match(/\[\d+\.\d+\.\d+]/g).length === 1;
+
+const oneVersionRegex =
+  /(?<prefix>#+ \[\d+\.\d+\.\d+].+)(?<content>(.|\r|\n)*?)(?=<#\s*\[\d+\.\d+\.\d+])/;
+const manyVersionsRegex =
+  /(?<prefix>#+ \[\d+\.\d+\.\d+].+)(?<content>(.|\r|\n)*?)(?<suffix>#+ \[\d+\.\d+\.\d+].+)/;
+const {
+  groups: { content },
+} = isFirstVersion
+  ? oneVersionRegex.exec(changelog)
+  : manyVersionsRegex.exec(changelog);
+
+const trimmedContent = content.trim();
+
+const lastVerChangelogPath = resolve(rootDir, "last-ver-changelog.txt");
+await writeFile(lastVerChangelogPath, trimmedContent, "utf-8");
