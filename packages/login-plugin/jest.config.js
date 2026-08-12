7a4df633fa98cd119716110f8679e7cac959c2f4
Harshit Panchal <harshit.panchal@sap.com>
2026-08-12 10:25:42 +0100

    fix(inquirer-gui): add tests, jest mapper, and move to peerDependencies for login-plugin

--- packages/login-plugin/jest.config.js
+++ packages/login-plugin/jest.config.js
@@ -2,4 +2,7 @@ const baseConfig = require("../../jest.config.base.js");
 
 module.exports = {
   ...baseConfig,
+  moduleNameMapper: {
+    "@sap-devx/inquirer-gui": "<rootDir>/../inquirer-gui/src/index.js",
+  },
 };
